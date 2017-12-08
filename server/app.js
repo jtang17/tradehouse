f90689066852ac1cc77a06e3f8c13658acb449e7
bkleung89 <bkleung89@gmail.com>
2017-12-08 09:16:21 -0800

    Connected to SQL; removed YouTube example files

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
+const store = require('../db/store.js');
+const streams = require('../db/index.js');
 
 app.use(express.static(path.join(__dirname, '/../client/public')));
 
