da74072b777a0b9f2b5cfd8da5e866d93ae1241f
bkleung89 <bkleung89@gmail.com>
2017-12-09 23:11:05 -0800

    Added back product routes.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const merchants = require('./merchant');
 const consumers = require('./consumer');
+const products = require('./product');
 
 module.exports = (app) => {
   app.use('/api/merchants', merchants);
   app.use('/api/consumers', consumers);
+  app.use('/api/products', products);
 };
