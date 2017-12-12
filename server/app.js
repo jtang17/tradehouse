3db29ffcf2723d74a98987b05127600ab3328166
bkleung89 <bkleung89@gmail.com>
2017-12-12 15:36:32 -0800

    Add socket.io boilerplate to server.

--- server/app.js
+++ server/app.js
@@ -3,13 +3,16 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('../models/index.js');
 const mountRoutes = require('./routes');
+
+const app = express();
 /* Webpack Hot Reloads */
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('../webpack.config');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-const app = express();
 const port = process.env.PORT || 5421;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,11 +40,15 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../client/public/index.html'));
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
 // TODO: prevent sync from outputting so much to console
 db.sequelize
   .sync()
   .then(() => {
-    app.listen(port, () => {
+    http.listen(port, () => {
       console.log(`Tradehouse server listening on port ${port}`);
     });
   });
