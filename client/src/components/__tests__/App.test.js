3ac95485d6e0f114e45cda786496bdec59b30851
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-12 23:13:55 -0500

    Add line to test circleCI functionality

--- client/src/components/__tests__/App.test.js
+++ client/src/components/__tests__/App.test.js
@@ -3,6 +3,7 @@ import {shallow, configure } from 'enzyme';
 import App from '../App.jsx';
 import Header from '../Header.jsx';
 
+
 // Enzyme clauses
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
