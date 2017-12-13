6d0906adc11dbf94eede52f7ab8cb416956c12e0
bkleung89 <bkleung89@gmail.com>
2017-12-12 17:18:19 -0800

    Move Socket.io boilerplate to CustomerView.jsx.

--- client/src/containers/CustomerView.jsx
+++ client/src/containers/CustomerView.jsx
@@ -4,18 +4,26 @@ import { connect } from 'react-redux';
 const ChannelID = 'UCSJ4gkVC6NrvII8umztf0Ow';
 
 const CustomerView = props => {
-    return (
-        <div>
-        Current Video: {props.video}
-        <br />
-        <iframe
-          width="560" height="315"
-          src={`https://www.youtube.com/embed/live_stream?channel=${ChannelID}&autoplay=1`}
-          frameBorder="0"
-          allowFullScreen>
-        </iframe>
-      </div>
-    )
+  return (
+    <div>
+      Current Video: {props.video}
+      <br />
+      <iframe
+        width="560" height="315"
+        src={`https://www.youtube.com/embed/live_stream?channel=${ChannelID}&autoplay=1`}
+        frameBorder="0"
+        allowFullScreen>
+      </iframe>
+      <ul id="messages"></ul>
+      <form action="">
+        <input id="m" autocomplete="off" /><button>Send</button>
+      </form>
+      <script src="/socket.io/socket.io.js"></script>
+      <script>
+        var socket = io();
+      </script>
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
