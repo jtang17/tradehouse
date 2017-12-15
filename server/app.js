06705ff711978f204aa8195c34a2adc5ad0dd818
bkleung89 <bkleung89@gmail.com>
2017-12-14 21:04:12 -0800

    Continue to experiment with Socket.io.

--- server/app.js
+++ server/app.js
@@ -40,8 +40,11 @@ app.get('/*', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  // socket.on('disconnect', () => {
+  //   console.log('user disconnected');
+  // });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
 });
 
