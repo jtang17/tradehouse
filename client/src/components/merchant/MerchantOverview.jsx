68430db5f646baf920af6a23ac680bd4a56276c9
bkleung89 <bkleung89@gmail.com>
2017-12-14 16:51:43 -0800

    Restore Anton's work that I previously deleted.

--- client/src/components/merchant/MerchantOverview.jsx
+++ client/src/components/merchant/MerchantOverview.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 
 const MerchantOverview = props => (
   <div className="mercOverview__container">
-    <h4>Company Overview</h4>
-    <div className="row">
-      <div className="col">
-          Company Name: {props.merchantInfo.username} <br />
-          Company Website: {props.merchantInfo.website} <br />
-          Company Ratings: {props.merchantInfo.rating} <br />
-          Company Location: {props.merchantInfo.location} <br />
-          Categories:<br />
-      </div>
-      <div className="col">
-          Support E-mail: {props.merchantInfo.email} <br />
-          Facebook: {props.merchantInfo.facebook} <br />
-          Twitter: {props.merchantInfo.twitter} <br />
-          Description: {props.merchantInfo.description} <br />
-      </div>
-    </div>
+    <h4 className="mercOverview__title"><span>Company Overview</span></h4>
+    <span className="mercOverview__profile">
+      <section className="mercOverview__profile--part1">
+        Company Name: {props.merchantInfo.username} <br />
+        Company Website: {props.merchantInfo.website} <br />
+        Company Ratings: {props.merchantInfo.rating} <br />
+        Company Location: {props.merchantInfo.location} <br />
+        Categories:<br />
+      </section>
+      <section className="mercOverview__profile--part2">
+        Categories:<br />
+        Support E-mail: {props.merchantInfo.email} <br />
+        Facebook: {props.merchantInfo.facebook} <br />
+        Twitter: {props.merchantInfo.twitter} <br />
+        Description: {props.merchantInfo.description} <br />
+      </section>
+    </span>
   </div>
 );
 
