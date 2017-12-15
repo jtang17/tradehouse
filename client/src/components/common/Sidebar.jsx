5bf3282bc7ecbeb495d7d88882e9e2ca397a1bfa
Jonathan Tang <j.tang17@gmail.com>
2017-12-15 12:20:29 -0800

    Modify sidebar to test CI

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -4,10 +4,11 @@ export default class Sidebar extends React.Component {
   render() {
     return (
       <div className="sidebar__container">
-        <a href="/browse">Browse Content</a>
+        <a href="/browse">Browse Content</a><br />
+        <a href="/profile">Change Profile</a>
         <br />
         <span>
-					This is going to work
+
         </span>
       </div>
     );
