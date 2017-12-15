84f56b1db8316841d8f732a48539a0e787a81508
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-15 18:55:53 -0500

    Import Customer and Merchant Sidebars, set up for conditional rendering

--- client/src/containers/Sidebar.jsx
+++ client/src/containers/Sidebar.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import CustomerSidebar from '../components/customer/CustomerSidebar.jsx';
+import MerchantSidebar from '../components/merchant/MerchantSidebar.jsx';
 
-export default class Sidebar extends React.Component {
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    // extrapolate to redux store
+    this.state = {
+      merchant: false,
+    };
+  }
+
   render() {
     return (
       <div className="sidebar__container">
-        <a href="/browse">Browse Content</a><br />
-        <a href="/profile">Change Profile</a>
-        <br />
-        <span>
-
-        </span>
+        {
+          this.state.merchant ? <MerchantSidebar /> : <CustomerSidebar />
+        }
       </div>
     );
   }
 }
+
+export default Sidebar;
