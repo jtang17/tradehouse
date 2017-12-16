c04addf8388dbf0ca6a478f70d7b4f1588bb55fe
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-15 19:38:27 -0500

    Create SidebarSearch component rendering just 'test'

---
+++ client/src/components/sidebar/SidebarSearch.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SidebarSearch extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Test</h1>
+      </div>
+    );
+  }
+}
+
+export default SidebarSearch;
