655f9c2334ac898a717b16375dd06dcbc01c4189
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-16 18:18:24 -0500

    Import fetchAllMerchants constants and build reducer for it

--- client/src/reducers/customerReducers.jsx
+++ client/src/reducers/customerReducers.jsx
@@ -1,6 +1,8 @@
 import {
   FETCH_CUSTOMER_SUCCESS,
   FETCH_CUSTOMER_FAILURE,
+  FETCH_ALL_MERCHANTS_SUCCESS,
+  FETCH_ALL_MERCHANTS_FAILURE
 } from '../actions/customerActions.jsx';
 
 export function customerInfo(state = {}, action) {
@@ -10,6 +12,20 @@ export function customerInfo(state = {}, action) {
     }
     case FETCH_CUSTOMER_FAILURE: {
       console.error(action.error);
+      return state;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export function allMerchants(state = [], action) {
+  switch (action.type) {
+    case FETCH_ALL_MERCHANTS_SUCCESS: {
+      return action.allMerchants;
+    }
+    case FETCH_ALL_MERCHANTS_FAILURE: {
       return state;
     }
     default: {
