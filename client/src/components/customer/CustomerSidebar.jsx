fbf44009d228ca24db8485aa94c2e7e367bf4302
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-15 19:17:22 -0500

    Build handleLeft/RightClick, bind, and call on click appropriately

--- client/src/components/customer/CustomerSidebar.jsx
+++ client/src/components/customer/CustomerSidebar.jsx
@@ -6,14 +6,24 @@ class CustomerSidebar extends React.Component {
     this.state = {
       leftTab: true,
     };
+    this.handleLeftTabClick = this.handleLeftTabClick.bind(this);
+    this.handleRightTabClick = this.handleRightTabClick.bind(this);
+  }
+
+  handleLeftTabClick() {
+    this.setState({leftTab: true});
+  }
+
+  handleRightTabClick() {
+    this.setState({leftTab: false});
   }
 
   render() {
     return (
       <div className="CustomerSidebar">
         <div className="tabs">
-          <h3 className="leftTab">Chat</h3>
-          <h3 className="rightTab">Search</h3>
+          <h3 className="leftTab" onClick={this.handleLeftTabClick}>Chat</h3>
+          <h3 className="rightTab" onClick={this.handleRightTabClick}>Search</h3>
         </div>
       </div>
     );
