807df852fa458c465ee22cb5306fb8a4b0aac45f
Jonathan Tang <j.tang17@gmail.com>
2017-12-16 14:14:47 -0800

    Remove obsolete TEST_ADD from jest tests

--- client/src/reducers/__tests__/cartReducers.test.js
+++ client/src/reducers/__tests__/cartReducers.test.js
@@ -1,18 +1,9 @@
 import { cart } from '../cartReducers.jsx';
-import { TEST_ADD } from '../../actions/cartActions.jsx';
 
 // TODO: Test async actions when they are ready.
 
 describe('cart reducers', () => {
   it('should return initial state', () => {
     expect(cart(null, {})).toBe(null);
-  });
-  it('TEST_ADD should add a product to the state', () => {
-    expect(cart(['cheese'], {type: TEST_ADD, product: 'cake'}).length).toBe(2);
-  });
-  it('TEST_ADD does not mutate original state', () => {
-    let originalState = ['chips'];
-    cart(originalState, {type: TEST_ADD, product: 'cake'});
-    expect(originalState.length).toBe(1);
   });
 })
