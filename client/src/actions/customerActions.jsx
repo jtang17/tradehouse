31a7951b42089fcc3320e89322a35d8cb84bc98f
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-18 18:28:02 -0500

    Remove fetchAllMerchants success and failure constants

--- client/src/actions/customerActions.jsx
+++ client/src/actions/customerActions.jsx
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 export const FETCH_CUSTOMER_SUCCESS = 'FETCH_CUSTOMER_SUCCESS';
 export const FETCH_CUSTOMER_FAILURE = 'FETCH_CUSTOMER_FAILURE';
-export const FETCH_ALL_MERCHANTS_SUCCESS = 'FETCH_ALL_MERCHANTS_SUCCESS';
-export const FETCH_ALL_MERCHANTS_FAILURE = 'FETCH_ALL_MERCHANTS_FAILURE';
 
 export const fetchCustomerLoading = bool => ({
   type: 'CUSTOMER_LOADING',
