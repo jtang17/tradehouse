04e010e30ea3b8e531b82e335e4f63d7a6735b97
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-18 18:38:11 -0500

    Import fetchAllMerchants and associated constants

--- client/src/actions/merchantActions.jsx
+++ client/src/actions/merchantActions.jsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export const FETCH_MERCHANT_SUCCESS = 'FETCH_MERCHANT_SUCCESS';
 export const FETCH_MERCHANT_FAILURE = 'FETCH_MERCHANT_FAILURE';
+export const FETCH_ALL_MERCHANTS_SUCCESS = 'FETCH_ALL_MERCHANTS_SUCCESS';
+export const FETCH_ALL_MERCHANTS_FAILURE = 'FETCH_ALL_MERCHANTS_FAILURE';
 
 export const fetchMerchantLoading = bool => ({
   type: 'MERCHANT_LOADING',
@@ -19,3 +21,13 @@ export const fetchMerchantInfo = ({ id }) => (dispatch) => {
       error: err,
     }));
 };
+
+export const fetchAllMerchants = () => dispatch => axios.get('/api/merchants')
+  .then(res => dispatch({
+    type: FETCH_ALL_MERCHANTS_SUCCESS,
+    allMerchants: res.data,
+  }))
+  .catch(err => dispatch({
+    type: FETCH_ALL_MERCHANTS_FAILURE,
+    error: err,
+  }));
