9235bf1345f5ae18c1a7d500a226fc5acff9dc7b
Jonathan Tang <j.tang17@gmail.com>
2017-12-21 11:13:17 -0800

    Add https to twitch embed url

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -14,7 +14,7 @@ class Featured extends React.Component {
           <h2>Featured Broadcast: Core Koans</h2>
         </Link>
         <iframe
-          src="http://player.twitch.tv/?channel=datjoncat&muted=true"
+          src="https://player.twitch.tv/?channel=datjoncat&muted=true"
           height="300"
           width="400"
           frameBorder="0"
@@ -26,7 +26,7 @@ class Featured extends React.Component {
           frameBorder="0"
           scrolling="no"
           id="chat_embed"
-          src="http://www.twitch.tv/embed/datjoncat/chat"
+          src="https://www.twitch.tv/embed/datjoncat/chat"
           height="400"
           width="200"
         />
