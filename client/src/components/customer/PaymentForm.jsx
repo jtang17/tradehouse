d36a533bb2f144ebe1051070dd2a33a874fadd0b
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-30 13:04:47 -0700

    Set up component for test rendering before building react form

---
+++ client/src/components/customer/PaymentForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class PaymentForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>PAYMENT FORM TEST</div>
+    );
+  }
+}
+
+export default PaymentForm;
