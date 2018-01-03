2096c80965b1a284c4eb89c96d5cb9c1b416cb9e
Charles Paul Bennett <cpbennett4@gmail.com>
2018-01-03 16:53:58 -0700

    Build stripe's checkout component

--- client/src/components/customer/PaymentForm.jsx
+++ client/src/components/customer/PaymentForm.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
+import axios from 'axios';
+import StripeCheckout from 'react-stripe-checkout';
 
-class PaymentForm extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+import STRIPE_CONSTS from '../../../../config.js';
 
-  componentDidMount() {
-  }
+const CURRENCY = 'USD';
 
-  render() {
-    return (
-      <div>PAYMENT FORM TEST</div>
-    );
-  }
-}
+const fromDollarToCent = amount => amount * 100;
+
+const successPayment = data => {
+  alert('Payment Successful');
+};
+
+const errorPayment = data => {
+  alert('Payment Error');
+};
+
+const onToken = (amount, description) => token =>
+  axios.post(STRIPE_CONSTS.PAYMENT_SERVER_URL,
+    {
+      description,
+      source: token.id,
+      currency: CURRENCY,
+      amount: fromDollarToCent(amount)
+    })
+    .then(successPayment)
+    .catch(errorPayment);
+
+const PaymentForm = ({ name, description, amount }) =>
+  <StripeCheckout
+    name={name}
+    description={description}
+    amount={fromDollarToCent(amount)}
+    token={onToken(amount, description)}
+    currency={CURRENCY}
+    stripeKey={STRIPE_CONSTS.STRIPE_PUBLISHABLE}
+  />
 
 export default PaymentForm;
