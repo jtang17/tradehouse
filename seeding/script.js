9d79ea42243d1a299845040e273b12ff8e7d0e92
AntonVS <InfiniteStyles.Create@gmail.com>
2018-01-03 12:32:40 -0800

    Refine mock data.

--- seeding/script.js
+++ seeding/script.js
@@ -4,6 +4,7 @@ const products = require('./mock_products--cars.json');
 const streams = require('./mock_streams.json');
 const merchants = require('./mock_merchants.json');
 const customers = require('./customers.json');
+const elastic = require('../models/elasticSearch.js');
 
 async function seed() {
   const merchantPromises = [];
@@ -22,12 +23,66 @@ async function seed() {
   const savedMerchants = await Promise.all(merchantPromises);
   const savedCustomers = await Promise.all(customerPromises);
 
+  for (var merchant of savedMerchants) {
+    elastic.index({
+      index: 'bgm_merchants',
+      type: 'merchant',
+      id: merchant.dataValues.id || 'Missing ID',
+      body: {
+        logo: merchant.dataValues.logo || 'Missing Logo',
+        username: merchant.dataValues.username || 'Missing Username',
+        website: merchant.dataValues.website || 'Missing Website',
+        rating: merchant.dataValues.rating || 0,
+        location: merchant.dataValues.location || 'Missing Location',
+        email: merchant.dataValues.email || 'Missing Email',
+        facebook: merchant.dataValues.facebook || 'Missing Facebook',
+        twitter: merchant.dataValues.twitter || 'Missing Twitter',
+        description: merchant.dataValues.description || 'Missing Description',
+        currentProduct: merchant.dataValues.currentProduct || 0,
+        storeName: merchant.dataValues.storeName || 'Missing StoreName',
+        sub: merchant.dataValues.sub || 1,
+        facebook: merchant.dataValues.facebook || 'Missing Facebook',
+        createdAt: merchant.dataValues.createdAt || null,
+        updatedAt: merchant.dataValues.updatedAt || null,
+      },
+    }, (err, res) => {
+      if (err) { console.error(err); } else {
+        console.log(`Added brand new registered Merchant to Elastic! ${res}`);
+      }
+    });
+  }
+
   for (let i = 0; i < products.length; i += 1) {
     const index = Math.floor(Math.random() * (Math.floor(savedMerchants.length)));
     productPromises.push(controllers.saveNewProduct(Object.assign(products[i], {
       // merchantId: savedMerchants[index].id,
       merchantId: 1,
     })));
+  }
+
+  const savedProducts = await Promise.all(productPromises);
+
+  for (var product of savedProducts) {
+    elastic.index({
+      index: 'bgm_products',
+      type: 'product',
+      id: product.dataValues.id,
+      body: {
+        id: product.dataValues.id || null,
+        title: product.dataValues.title || 'Unnamed Product',
+        description: product.dataValues.description || 'Missing Description',
+        quantity: product.dataValues.quantity || 'Missing Quantity',
+        unitPrice: product.dataValues.unitPrice || 'Missing Price',
+        merchantId: product.dataValues.merchantId || 1,
+        imageUrl: product.dataValues.imageUrl || 'Missing Image',
+        createdAt: product.dataValues.createdAt || null,
+        updatedAt: product.dataValues.updatedAt || null,
+      },
+    }, (err, res) => {
+      if (err) { console.error(err); } else {
+        console.log(`Added new item to Elastic! ${res}`);
+      }
+    });
   }
 
   for (let i = 0; i < streams.length; i += 1) {
