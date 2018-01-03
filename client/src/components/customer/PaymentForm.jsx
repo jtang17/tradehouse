88a15de4ac63973eccaaaea6477bc6bbae5b35ab
Charles Paul Bennett <cpbennett4@gmail.com>
2018-01-03 16:59:24 -0700

    Run lint

--- client/src/components/customer/PaymentForm.jsx
+++ client/src/components/customer/PaymentForm.jsx
@@ -8,33 +8,35 @@ const CURRENCY = 'USD';
 
 const fromDollarToCent = amount => amount * 100;
 
-const successPayment = data => {
+const successPayment = (data) => {
   alert('Payment Successful');
 };
 
-const errorPayment = data => {
+const errorPayment = (data) => {
   alert('Payment Error');
 };
 
 const onToken = (amount, description) => token =>
-  axios.post(STRIPE_CONSTS.PAYMENT_SERVER_URL,
+  axios.post(
+    STRIPE_CONSTS.PAYMENT_SERVER_URL,
     {
       description,
       source: token.id,
       currency: CURRENCY,
-      amount: fromDollarToCent(amount)
-    })
+      amount: fromDollarToCent(amount),
+    },
+  )
     .then(successPayment)
     .catch(errorPayment);
 
 const PaymentForm = ({ name, description, amount }) =>
-  <StripeCheckout
+  (<StripeCheckout
     name={name}
     description={description}
     amount={fromDollarToCent(amount)}
     token={onToken(amount, description)}
     currency={CURRENCY}
     stripeKey={STRIPE_CONSTS.STRIPE_PUBLISHABLE}
-  />
+  />);
 
 export default PaymentForm;
