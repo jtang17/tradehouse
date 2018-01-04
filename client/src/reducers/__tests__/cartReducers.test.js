9641042674da333565060e588d3a8a439cde1708
bkleung89 <bkleung89@gmail.com>
2018-01-04 12:55:32 -0800

    Adjust indentation and update reducer tests.

--- client/src/reducers/__tests__/cartReducers.test.js
+++ client/src/reducers/__tests__/cartReducers.test.js
@@ -10,30 +10,30 @@ describe('cart reducers', () => {
   });
 
   it('failing to add an item to the cart should return initial cart state', () => {
-  	const addToCartFail = {
-  		type: actions.ADD_TO_CART_FAILURE
-  	}
-  	expect(cart(['sweater'], addToCartFail)).toEqual(['sweater']);
+    const addToCartFail = {
+      type: actions.ADD_TO_CART_FAILURE
+    }
+    expect(cart(['sweater'], addToCartFail)).toEqual(['sweater']);
   });
 
   it('failing to remove an item from the cart should return initial cart state', () => {
-  	const removeFromCartFail = {
-  		type: actions.REMOVE_FROM_CART_FAILURE
-  	}
-  	expect(cart(['sweater'], removeFromCartFail)).toEqual(['sweater']);
+    const removeFromCartFail = {
+      type: actions.REMOVE_FROM_CART_FAILURE
+    }
+    expect(cart(['sweater'], removeFromCartFail)).toEqual(['sweater']);
   });
 
   it('failing to fetch an item from the cart should return initial cart state', () => {
-  	const fetchFromCartFail = {
-  		type: actions.FETCH_CART_FAILURE
-  	}
-  	expect(cart(['sweater'], fetchFromCartFail)).toEqual(['sweater']);
+    const fetchFromCartFail = {
+      type: actions.FETCH_CART_FAILURE
+    }
+    expect(cart(['sweater'], fetchFromCartFail)).toEqual(['sweater']);
   });
 
   it('failing to decrease quantity from the cart should return initial cart state', () => {
-  	const fetchFromCartFail = {
-  		type: actions.FETCH_CART_FAILURE
-  	}
-  	expect(cart(['sweater'], {type: actions.FETCH_CART_FAILURE})).toEqual(['sweater']);
+    const fetchFromCartFail = {
+      type: actions.FETCH_CART_FAILURE
+    }
+    expect(cart(['sweater'], {type: actions.FETCH_CART_FAILURE})).toEqual(['sweater']);
   });
 })
--- client/src/reducers/__tests__/productReducers.test.js
+++ client/src/reducers/__tests__/productReducers.test.js
@@ -1,50 +1,48 @@
 import * as actions from '../../actions/productActions.jsx';
 
-import { products } from '../productReducers.jsx';
+import { merchantProducts } from '../productReducers.jsx';
 
 describe('tests for product reducers', () => {
-
-
-	it('should sucessfully add a product to the store', () => {
-		const addProductSuccess = {
-			type: actions.ADD_PRODUCT_SUCCESS,
-			product: 'Yellow Sweatshirt',
-		};
-		expect(products([], addProductSuccess)).toEqual(['Yellow Sweatshirt']);
-	});
-	it('should fail adding a product to the store', () => {
-		const addProductFail = {
-			type: actions.ADD_PRODUCT_FAILURE,
-			product: 'Yellow Sweatshirt',
-		};
-		expect(products([], addProductFail)).toEqual([]);
-	});
-	it('should fail deleting a product to the state', () => {
-		const deleteProductFail = {
-			type: actions.DELETE_PRODUCT_FAILURE,
-			product: 'Yellow Sweatshirt',
-		};
-		expect(products([], deleteProductFail)).toEqual([]);
-	});
-	it('should fail fetching all products from the store', () => {
-		const fetchProductFail = {
-			type: actions.FETCH_ALL_PRODUCT_FAILURE,
-			product: 'Yellow Sweatshirt',
-		};
-		expect(products([], fetchProductFail)).toEqual([]);
-	});
-	it('should fail fetching a single merchants product from the store', () => {
-		const fetchMercProductFail = {
-			type: actions.FETCH_MERCHANT_PRODUCTS_FAILURE,
-			product: 'Yellow Sweatshirt',
-		};
-		expect(products([], fetchMercProductFail)).toEqual([]);
-	});
-	it('should fail fetching a single product from the store', () => {
-		const fetchSingleProductFail = {
-			type: actions.FETCH_SINGLE_PRODUCT_FAILURE,
-			product: 'Yellow Sweatshirt',
-		};
-		expect(products([], fetchSingleProductFail)).toEqual([]);
-	});
+  it('should successfully add a product to the store', () => {
+    const addProductSuccess = {
+      type: actions.ADD_MERCHANT_PRODUCT_SUCCESS,
+      product: 'Yellow Sweatshirt',
+    };
+    expect(products([], addProductSuccess)).toEqual(['Yellow Sweatshirt']);
+  });
+  it('should fail adding a product to the store', () => {
+    const addProductFail = {
+      type: actions.ADD_MERCHANT_PRODUCT_FAILURE,
+      product: 'Yellow Sweatshirt',
+    };
+    expect(products([], addProductFail)).toEqual([]);
+  });
+  it('should fail deleting a product to the state', () => {
+    const deleteProductFail = {
+      type: actions.DELETE_MERCHANT_PRODUCT_FAILURE,
+      product: 'Yellow Sweatshirt',
+    };
+    expect(products([], deleteProductFail)).toEqual([]);
+  });
+  it('should fail fetching all products from the store', () => {
+    const fetchProductFail = {
+      type: actions.FETCH_ALL_PRODUCTS_FAILURE,
+      product: 'Yellow Sweatshirt',
+    };
+    expect(products([], fetchProductFail)).toEqual([]);
+  });
+  it('should fail fetching a single merchants product from the store', () => {
+    const fetchMercProductFail = {
+      type: actions.FETCH_MERCHANT_PRODUCTS_FAILURE,
+      product: 'Yellow Sweatshirt',
+    };
+    expect(products([], fetchMercProductFail)).toEqual([]);
+  });
+  it('should fail fetching a single product from the store', () => {
+    const fetchSingleProductFail = {
+      type: actions.FETCH_SINGLE_PRODUCT_FAILURE,
+      product: 'Yellow Sweatshirt',
+    };
+    expect(products([], fetchSingleProductFail)).toEqual([]);
+  });
 });
