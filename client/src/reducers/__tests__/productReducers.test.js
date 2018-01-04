b9ccba072a640d6f71f5af99bd6c59160b34cc33
bkleung89 <bkleung89@gmail.com>
2018-01-04 12:57:57 -0800

    Update reducer tests to reflect reducer changes.

--- client/src/reducers/__tests__/productReducers.test.js
+++ client/src/reducers/__tests__/productReducers.test.js
@@ -8,41 +8,41 @@ describe('tests for product reducers', () => {
       type: actions.ADD_MERCHANT_PRODUCT_SUCCESS,
       product: 'Yellow Sweatshirt',
     };
-    expect(products([], addProductSuccess)).toEqual(['Yellow Sweatshirt']);
+    expect(merchantProducts([], addProductSuccess)).toEqual(['Yellow Sweatshirt']);
   });
   it('should fail adding a product to the store', () => {
     const addProductFail = {
       type: actions.ADD_MERCHANT_PRODUCT_FAILURE,
       product: 'Yellow Sweatshirt',
     };
-    expect(products([], addProductFail)).toEqual([]);
+    expect(merchantProducts([], addProductFail)).toEqual([]);
   });
   it('should fail deleting a product to the state', () => {
     const deleteProductFail = {
       type: actions.DELETE_MERCHANT_PRODUCT_FAILURE,
       product: 'Yellow Sweatshirt',
     };
-    expect(products([], deleteProductFail)).toEqual([]);
+    expect(merchantProducts([], deleteProductFail)).toEqual([]);
   });
   it('should fail fetching all products from the store', () => {
     const fetchProductFail = {
       type: actions.FETCH_ALL_PRODUCTS_FAILURE,
       product: 'Yellow Sweatshirt',
     };
-    expect(products([], fetchProductFail)).toEqual([]);
+    expect(merchantProducts([], fetchProductFail)).toEqual([]);
   });
   it('should fail fetching a single merchants product from the store', () => {
     const fetchMercProductFail = {
       type: actions.FETCH_MERCHANT_PRODUCTS_FAILURE,
       product: 'Yellow Sweatshirt',
     };
-    expect(products([], fetchMercProductFail)).toEqual([]);
+    expect(merchantProducts([], fetchMercProductFail)).toEqual([]);
   });
   it('should fail fetching a single product from the store', () => {
     const fetchSingleProductFail = {
       type: actions.FETCH_SINGLE_PRODUCT_FAILURE,
       product: 'Yellow Sweatshirt',
     };
-    expect(products([], fetchSingleProductFail)).toEqual([]);
+    expect(merchantProducts([], fetchSingleProductFail)).toEqual([]);
   });
 });
